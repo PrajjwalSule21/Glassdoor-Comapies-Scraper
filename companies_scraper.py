65a341e4deed2adee5006b078f000a0422388073
Prajjwal Sule <81765165+PrajjwalSule21@users.noreply.github.com>
2023-06-02 12:30:44 +0530

    Update companies_scraper.py

--- companies_scraper.py
+++ companies_scraper.py
@@ -9,7 +9,7 @@ def page_authentication(url):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
     response = requests.get(url, headers=headers)
     if response.status_code < 200 or response.status_code > 299:
-        raise Exception('Failed to load the page {}').format(url)
+        raise Exception(f'Failed to load the page {url}')
     page_content = response.text
     webpage =  BeautifulSoup(page_content, 'html.parser')
     return webpage
